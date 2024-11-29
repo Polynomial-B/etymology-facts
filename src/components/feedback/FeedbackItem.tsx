ee6bd2ad05dd9fe80ca98c5f81079dc11578f724
Matt <mphlamb@gmail.com>
2024-11-29 12:11:48 +0100

    like upvote logic added (local only)

--- src/components/feedback/FeedbackItem.tsx
+++ src/components/feedback/FeedbackItem.tsx
@@ -1,21 +1,25 @@
 import { TriangleUpIcon } from "@radix-ui/react-icons";
 import { FeedbackItemProps } from "../../lib/types";
+import { useState } from "react";
 
 export default function FeedbackItem({
 	feedbackItem,
 }: FeedbackItemProps): React.JSX.Element {
+	const [open, setOpen] = useState<boolean>(false);
+	const [like, setLike] = useState(feedbackItem.upvoteCount);
 	const { upvoteCount, badgeInitials, language, text, daysAgo } =
 		feedbackItem;
+
 	return (
-		<li className="feedback">
-			<button>
+		<li className={`feedback ${open ? "feedback--expand" : ""}`}>
+			<button onClick={() => setLike((prev) => ++prev)}>
 				<TriangleUpIcon />
-				<span>{upvoteCount}</span>
+				<span>{like}</span>
 			</button>
 			<div>
 				<p>{badgeInitials}</p>
 			</div>
-			<div>
+			<div onClick={() => setOpen((prev) => !prev)}>
 				<p>{language}</p>
 				<p>{text}</p>
 			</div>
